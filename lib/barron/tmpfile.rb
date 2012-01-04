a34dddcb29f2bc3f70337ab196186370cb041bdc
Matthew Beale <matt.beale@madhatted.com>
2012-01-04 15:16:28 -0500

    Add a quiet mode, set perms at the last minute

--- lib/barron/tmpfile.rb
+++ lib/barron/tmpfile.rb
@@ -11,12 +11,19 @@ module Barron::Tmpfile
       }.merge(options)
       value = nil
       open("#{options[:tmpdir]}/barron-#{Digest::MD5.hexdigest("#{name}")}", File::RDWR|File::CREAT, 0666) do |f|
-        f.chmod(0666)
         begin
           if options[:block]
             f.flock(File::LOCK_EX)
+            begin
+              f.chmod(0666)
+            rescue Errno::EPERM
+            end
             value = yield f
           elsif f.flock(File::LOCK_EX|File::LOCK_NB)
+            begin
+              f.chmod(0666)
+            rescue Errno::EPERM
+            end
             value = yield f
           end
         ensure
